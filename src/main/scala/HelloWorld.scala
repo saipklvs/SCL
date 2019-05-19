bcc08c4e2bc705d64c5813adac54287f2f09e5d0
pavansaikumar <37949891+pavansaikumar@users.noreply.github.com>
2019-05-19 00:47:26 -0500

    Text file added

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,3 +1,13 @@
+/*Importing the packeafes*/
+
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+
+
 object HelloWorld extends App{
   println("Hello World!")
   println(2+3)
